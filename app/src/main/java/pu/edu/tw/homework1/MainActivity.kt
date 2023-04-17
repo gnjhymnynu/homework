0faa9a07416a4701bf419fc2def457ba6279335d
gnjhymnynu <s1102295@gm.pu.edu.tw>
2023-04-17 18:54:24 +0800

    作業

--- app/src/main/java/pu/edu/tw/homework1/MainActivity.kt
+++ app/src/main/java/pu/edu/tw/homework1/MainActivity.kt
@@ -1,7 +1,9 @@
 package pu.edu.tw.homework1
 
 import android.graphics.Color
+import android.graphics.Rect
 import android.graphics.Typeface
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.GestureDetector
@@ -9,18 +11,29 @@ import android.view.GestureDetector.OnGestureListener
 import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnClickListener
+import android.view.View.OnTouchListener
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import java.util.jar.Pack200
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
-class MainActivity : AppCompatActivity(), OnGestureListener {
+class MainActivity : AppCompatActivity(), OnGestureListener, OnTouchListener {
 
     lateinit var txv: TextView
     lateinit var txv2: TextView
     lateinit var btn: Button
     lateinit var gDetector: GestureDetector
     var count:Int = 0
+    lateinit var img1: ImageView
+    lateinit var img2: ImageView
+    lateinit var img3: ImageView
+    lateinit var mper: MediaPlayer
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +49,17 @@ class MainActivity : AppCompatActivity(), OnGestureListener {
 
         btn = findViewById(R.id.btn)
         gDetector = GestureDetector(this, this)
+        img1 = findViewById(R.id.img1)
+        img2 = findViewById(R.id.img2)
+        img3 = findViewById(R.id.img3)
+        img1.setOnTouchListener(this)
+        img2.setOnTouchListener(this)
+        img3.visibility = View.GONE
+        mper = MediaPlayer()
+        mper = MediaPlayer.create(this, R.raw.scenario)
+        mper.start()
+
+
     }
 
     fun happy(v: View){
@@ -86,7 +110,7 @@ class MainActivity : AppCompatActivity(), OnGestureListener {
     }
 
     override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
-        if (e1.x <= e2.x){
+       /* if (e1.x <= e2.x){
             txv.text = "往右快滑"
             count++
             if(count>2){
@@ -104,6 +128,37 @@ class MainActivity : AppCompatActivity(), OnGestureListener {
         var res:Int = getResources().getIdentifier("pu" + count.toString(),
             "drawable", getPackageName())
         findViewById<LinearLayout>(R.id.bg).setBackgroundResource(res)
+*/
+
+        return true
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        if (v==img1){
+            txv.text = "精靈1"
+        }
+        else{
+            txv.text = "精靈2"
+        }
+
+        if (event?.action == MotionEvent.ACTION_MOVE){
+            v?.x = event.rawX- v!!.width/2
+            v?.y = event.rawY- v!!.height/2
+        }
+
+        var r1: Rect = Rect(img1.x.toInt(), img1.y.toInt(),
+            img1.x.toInt() + img1.width, img1.y.toInt() + img1.height)
+        var r2: Rect = Rect(img2.x.toInt(), img2.y.toInt(),
+            img2.x.toInt() + img2.width, img2.y.toInt() + img2.height)
+
+        if(r1.intersect(r2)) {
+            txv.text = "碰撞"
+            img3.visibility = View.VISIBLE
+        }
+        else{
+            img3.visibility = View.GONE
+        }
+
 
 
         return true
